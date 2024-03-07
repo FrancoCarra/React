1dd8e228b0a1cbcfc714eebb1c754364832e415d
franco carra <francocarra6@gmail.com>
2024-03-07 09:50:10 -0500

    add Local Storage

--- projects/01React-project/src/App.jsx
+++ projects/01React-project/src/App.jsx
@@ -8,11 +8,16 @@ import { WinnerModal } from './components/WinnerModal'
 
 
 export function App() {
-    const [board, setBoard] = useState(
-        Array(9).fill(null)
-        )
-
-const [turn,setTurn] = useState(TURNS.X)
+    const [board, setBoard] = useState(() => {
+        const boardFromStorage = window.localStorage.getItem('board')
+        if(boardFromStorage) return JSON.parse(boardFromStorage)
+        return Array(9).fill(null)
+        
+    })
+const [turn,setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+})
 const [winner, setWinner] = useState(null)
 
 
@@ -20,6 +25,10 @@ const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+
 }
 
 const updateBoard = (index) => {
@@ -31,6 +40,8 @@ const updateBoard = (index) => {
     setBoard(newBoard)
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X 
     setTurn(newTurn)
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
     const newWinner = checkWinnerFrom(newBoard)
     if(newWinner) {
         confetti()
--- projects/01React-project/src/constants.js
+++ projects/01React-project/src/constants.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export const TURNS = {
     X: 'x',
     O: 'o'
